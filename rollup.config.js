24c17e8cc4936e9725898a4cd04aaefe137d8838
Thomas Judd-Cooper <me@tomjuddcooper.co.uk>
2019-04-11 17:21:28 +0100

    Chore: update dependencies, eject from react-scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-console */
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import resolve from 'rollup-plugin-node-resolve';
 import url from 'rollup-plugin-url';
 import svgr from '@svgr/rollup';
-import copy from 'rollup-plugin-copy';
-import cleaner from 'rollup-plugin-cleaner';
-
+import commonjs from 'rollup-plugin-commonjs';
+import fs from 'fs-extra';
+import path from 'path';
 import pkg from './package.json';
+
+console.info('Prebuild...');
+fs.removeSync(path.join(__dirname, './lib/'));
+fs.mkdirSync(path.join(__dirname, './lib/'));
+fs.copySync(
+  path.join(__dirname, './src/styles'),
+  path.join(__dirname, './lib/styles')
+);
+console.info('Done');
 
 export default {
   input: 'src/index.js',
@@ -25,9 +34,6 @@ export default {
     }
   ],
   plugins: [
-    cleaner({
-      targets: ['./dist/*', './lib/*']
-    }),
     external(),
     postcss({
       modules: true
@@ -35,12 +41,7 @@ export default {
     url(),
     svgr(),
     babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers']
-    }),
-    copy({
-      'src/styles/': 'lib/styles/',
-      verbose: true
+      exclude: 'node_modules/**'
     }),
     resolve({ extensions: ['.mjs', '.js', '.json', '.node', '.jsx'] }),
     commonjs()
