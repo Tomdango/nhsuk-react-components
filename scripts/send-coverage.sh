3a9b69d8345f9b94ef3928b756a87f59c3f192aa
Thomas Judd-Cooper <me@tomjuddcooper.co.uk>
2019-06-20 14:45:09 +0100

    Fix useless use of Cat

--- scripts/send-coverage.sh
+++ scripts/send-coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "prod" ]; then
-  cat ./coverage/lcov.info | ./node_modules/.bin/codacy-coverage
+  < ./coverage/lcov.info tr ' ' _ | ./node_modules/.bin/codacy-coverage
 fi
