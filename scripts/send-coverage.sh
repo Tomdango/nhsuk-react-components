438cb3748b33b904badd79ae197b6b6b1aaf8ecf
Luke Pearson <luke.pearson@infinityworks.com>
2020-01-23 17:31:16 +0000

    Dont fail build if coverage fails to upload

--- scripts/send-coverage.sh
+++ scripts/send-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-  < ./coverage/lcov.info tr ' ' _ | ./node_modules/.bin/codacy-coverage
+    < ./coverage/lcov.info tr ' ' _ | ./node_modules/.bin/codacy-coverage || true
 else
     echo "Not Sending Coverage..."
 fi
